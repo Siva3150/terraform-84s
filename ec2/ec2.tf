9ec3c77630873815263f27bb69431f02be62a1a8
Siva prasad <sivaprasad3150@gmail.com>
2025-06-10 00:29:16 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "roboshop" {
+    instance_type = "t3.micro"
+    ami = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+    tags = {
+        name = "roboshop"
+    }
+}
+
+
+
+
+resource "aws_security_group" "allow_all" {
+name = "allow_all"
+description = "allows traffic from all ports"
+    
+
+ingress {
+    from_port = 0 
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+}
+
+egress {
+    from_port = 0 
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+}
+
+tags = {
+    Name = "allow_all"
+
+}
+
+}
---
+++ variables/variables.tf
@@ -0,0 +1,53 @@
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "ami_id" {
+    type = string 
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of joindevops RHEL9"
+}
+ variable "ec2_tags" {
+    type = map(string)
+    default = {
+        Name = "Roboshop"
+        purpose = "variables-demo"
+
+    }
+ }
+
+ variable "sg_name" {
+     default = "vars-file-allow-all"
+ }
+
+ variable "sg_description" {
+   default = "allowing all ports from internet"
+ }
+
+ variable "from_port" {
+    type = number
+    default = 0
+}
+
+ variable "to_port" {
+    type = number 
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
+
+
+
+
+
+
