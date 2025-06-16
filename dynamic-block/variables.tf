468388ef3278b1f50cd65c74eb2c45ab2cafdf6f
Siva prasad <sivaprasad3150@gmail.com>
2025-06-16 18:19:05 +0530

    user-profile

--- dynamic-block/variables.tf
+++ dynamic-block/variables.tf
@@ -1,72 +1,65 @@
 variable "instance_type" {
-  type    = string
-  default = "t3.micro"
+    type = string
+    default = "t3.micro"
 }
 
 variable "ami_id" {
-  type        = string
-  default     = "ami-09c813fb71547fc4f"
-  description = "AMI ID of joindevops RHEL9"
+    type = string 
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of joindevops RHEL9"
 }
-variable "ec2_tags" {
-  type = map(string)
-  default = {
-    Name    = "Roboshop"
-    purpose = "variables-demo"
+ variable "ec2_tags" {
+    type = map(string)
+    default = {
+        Name = "Roboshop"
+        purpose = "variables-demo"
 
-  }
+    }
+ }
+
+ variable "sg_name" {
+     default = "vars-file-allow-all"
+ }
+
+ variable "sg_description" {
+   default = "allowing all ports from internet"
+ }
+
+ variable "from_port" {
+    type = number
+    default = 0
 }
 
-variable "sg_name" {
-  default = "vars-file-allow-all"
-}
-
-variable "sg_description" {
-  default = "allowing all ports from internet"
-}
-
-variable "from_port" {
-  type    = number
-  default = 0
-}
-
-variable "to_port" {
-  type    = number
-  default = 0
+ variable "to_port" {
+    type = number 
+    default = 0
 }
 
 variable "cidr_blocks" {
-  type    = list(string)
-  default = ["0.0.0.0/0"]
+    type = list(string)
+    default = ["0.0.0.0/0"]
 }
 
 variable "sg_tags" {
-  default = {
-    Name = "allow-all"
-  }
+    default = {
+        Name = "allow-all"
+    }
 }
 
 variable "ingress_ports" {
-  default = [ # list(map)
-    {
-      from_port = 22
-      to_port   = 22
+  default = [
 
+    {
+      from_port =22
+      to_port = 22
     },
     {
       from_port = 80
-      to_port   = 80
+      to_port = 80
     },
     {
-      from_port = 8080
-      to_port   = 8080
+    from_port = 8080
+    to_port = 8080
     }
   ]
-
 }
-
-
-
-
-
-
